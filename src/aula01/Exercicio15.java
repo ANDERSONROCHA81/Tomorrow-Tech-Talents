58b659afa4a68c9490a26d6a574966cb9d64d8f5
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-04-09 00:40:59 -0300

    Criação da classe Exercicio15

---
+++ src/aula01/Exercicio15.java
@@ -0,0 +1,19 @@
+package aula01;
+
+/*
+* Escreva um programa para calcular a quantidade de ferraduras
+necessárias para equipar todos os cavalos comprados para um haras.
+A informação de cavalos comprados deve ser informada pelo usuário.*/
+
+import java.util.Scanner;
+
+public class Exercicio15 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe a quantidade de cavalos comprados:");
+        int quantidadeDeCavalos = entrada.nextInt();
+
+        System.out.printf("A quantidade de ferraduras necessárias para equipar todos os %d cavalos é de %d\n", quantidadeDeCavalos, quantidadeDeCavalos * 4);
+    }
+}
