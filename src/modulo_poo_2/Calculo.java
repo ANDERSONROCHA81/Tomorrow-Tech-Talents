94a1119bdcd5117dcfee61263558f2721d697140
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-05-23 22:17:46 -0300

    Adição do método calcular e exclusão dos demais métodos

--- src/modulo_poo_2/Calculo.java
+++ src/modulo_poo_2/Calculo.java
@@ -1,8 +1,5 @@
 package modulo_poo_2;
 
 public interface Calculo {
-    double somar();
-    double subtrair();
-    double multiplicar();
-    double dividir();
+    double calcular(double ... valor);
 }
