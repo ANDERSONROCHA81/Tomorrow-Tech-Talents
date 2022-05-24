e49f5ece5fbdcf48597517ad182d309e5cb7567a
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-05-23 23:00:20 -0300

    Criação da classe Calculadora com implementação da opção somar

---
+++ src/modulo_poo_2/Calculadora.java
@@ -0,0 +1,39 @@
+package modulo_poo_2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double resultado = 0;
+
+        int opcao;
+
+        do {
+            System.out.println("==========Calculadora==========");
+            System.out.println("1 - Somar\n2 - Subtrair\n3 - Multiplicar\n4 - Dividir");
+            System.out.println("===============================");
+            System.out.println("Escolha qual das operações acima você deseja realizar:");
+            opcao = entrada.nextInt();
+
+            if (opcao == 1){
+                Soma soma = new Soma();
+                int escolha;
+                System.out.println("Informe quantos números deseja somar:");
+                escolha = entrada.nextInt();
+                double[] numeros = new double[escolha];
+
+                for (int i = 0; i < escolha; i++) {
+                    System.out.printf("Informe o %dº número:\n", i + 1);
+                    numeros[i] = entrada.nextDouble();
+                }
+
+               resultado = soma.calcular(numeros);
+                System.out.printf("O resultado da soma dos números informados é %.2f\n", resultado);
+            }
+
+        }while (opcao != 5);
+    }
+}
