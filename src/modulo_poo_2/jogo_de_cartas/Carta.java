96fcf51f03b08ae4a14bd119ff34cd32766637ae
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-05-29 23:17:06 -0300

    Atualização da classe Carta para implementar os métodos da interface ICarta

--- src/modulo_poo_2/jogo_de_cartas/Carta.java
+++ src/modulo_poo_2/jogo_de_cartas/Carta.java
@@ -1,6 +1,6 @@
 package modulo_poo_2.jogo_de_cartas;
 
-public class Carta {
+public class Carta implements ICarta{
     private String nome;
     private TipoDeCarta tipo;
     private int custo;
@@ -9,5 +9,20 @@ public class Carta {
         this.nome = nome;
         this.tipo = tipo;
         this.custo = custo;
+    }
+
+    @Override
+    public String getNome() {
+        return this.nome;
+    }
+
+    @Override
+    public TipoDeCarta getTipo() {
+        return this.tipo;
+    }
+
+    @Override
+    public int getCusto() {
+        return this.custo;
     }
 }
