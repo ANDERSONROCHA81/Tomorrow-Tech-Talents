155e4a4179775938c6af8aae1692d9ad01f318de
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-06-09 22:42:35 -0300

    Criação da classe Brinquedo

---
+++ src/modulo_poo_2/sistema_de_livraria/Brinquedo.java
@@ -0,0 +1,25 @@
+package modulo_poo_2.sistema_de_livraria;
+
+public class Brinquedo extends Produto{
+    private String tipo;
+
+    public Brinquedo(String nome, double preco, int quantidade, String tipo) {
+        super(nome, preco, quantidade);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Brinquedo{" +
+                "tipo='" + tipo + '\'' +
+                '}';
+    }
+}
