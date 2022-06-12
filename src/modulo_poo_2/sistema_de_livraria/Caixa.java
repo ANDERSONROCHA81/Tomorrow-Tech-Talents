11b0099cc2e85ea91cd250e0fc27d68cd45dacf3
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-06-11 22:20:23 -0300

    Criação das classes Estoque e Caixa

---
+++ src/modulo_poo_2/sistema_de_livraria/Caixa.java
@@ -0,0 +1,13 @@
+package modulo_poo_2.sistema_de_livraria;
+
+public class Caixa implements ICaixa {
+    @Override
+    public void adicionarDinheiroAoCaixa() {
+
+    }
+
+    @Override
+    public void mostrarDinheiroNoCaixa() {
+
+    }
+}
---
+++ src/modulo_poo_2/sistema_de_livraria/Estoque.java
@@ -0,0 +1,38 @@
+package modulo_poo_2.sistema_de_livraria;
+
+public class Estoque implements IEstoque {
+    @Override
+    public void adicionarProduto(Produto produto) {
+
+    }
+
+    @Override
+    public void removerProduto(Produto produto) {
+
+    }
+
+    @Override
+    public void alterarProduto(Produto produto) {
+
+    }
+
+    @Override
+    public void verProduto(Produto produto) {
+
+    }
+
+    @Override
+    public void listarProdutosDoMesmoTipo(Produto produto) {
+
+    }
+
+    @Override
+    public void listarProdutosDoEstoque() {
+
+    }
+
+    @Override
+    public void listarProdutosPorCategoria() {
+
+    }
+}
